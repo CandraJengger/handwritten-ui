754809e422d3a0fc19b8b5556f35e48585d22b9a
CandraJengger <cakjengger@gmail.com>
2026-04-17 11:12:22 +0700

    chore: bump version to 1.0.5 and remove unused ESM path utilities in add command

--- src/commands/add.ts
+++ src/commands/add.ts
@@ -1,10 +1,6 @@
 import fs from 'fs-extra';
 import path from 'path';
 import chalk from 'chalk';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 export default async function add(component: string) {
   if (component === 'button') {
